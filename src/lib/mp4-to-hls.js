23875ba8a4be87575ccaf5a2de89f7b4f0313918
Leslie Wong <79917148leslie@gmail.com>
2023-02-12 09:52:26 +0800

    Revert - unlink error must because of wrong files

--- src/lib/mp4-to-hls.js
+++ src/lib/mp4-to-hls.js
@@ -71,9 +71,8 @@ export default (filePath, tempDir = "", anilistID, fileName) => {
     ).stdout;
     !stdLog && console.log(`wirte file ${tempIndexM3u8Path} & ${temp10sTsPath}`);
 
-    if (fs.existsSync(tempIndexTsPath)) {
-      fs.unlinkSync(tempIndexTsPath);
-    }
+    fs.unlinkSync(tempIndexTsPath);
+
     console.log(`Remove file ${tempIndexTsPath}`);
 
     return tempPath;
